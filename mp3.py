a34f42ce87d2517a7a8606a2c11fbfee842debc7
nitinskumavat <nitin.s.kumavat@gmail.com>
2019-01-27 21:53:24 +0530

    Able to parse the string with multiple spaces in between including empty string with multiple spaces and converted process into different functions.

--- mp3.py
+++ mp3.py
@@ -2,40 +2,59 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import urllib.request
 import os
+import re
 
 #creating director to add downloaded song
 if not os.path.exists('songs'):
     os.makedirs('songs')
 
-#link for website
-site="http://search.chiasenhac.vn/search.php?s="
-song=input("Enter song name")
-song=song.replace(" ","+")
-site=site+song
-page = urlopen(site)
-soup = BeautifulSoup(page,'html.parser')
+def song_name():
+    #input string and convert multiple spaces to single space
+    song=re.sub(" +", " ", input("Enter song name\n"))
+    if not song.strip():
+        #strip because user might had entered only white spaces
+        print("Input is empty\n")
+    else:
+        site="http://search.chiasenhac.vn/search.php?s="
+        #replace " " with '+' as per website link
+        song=song.replace(" ","+")
+        site=site+song
+        parse_site(site)
+    
+def parse_site(site):
+    page = urlopen(site)
+    soup = BeautifulSoup(page,'html.parser')
+    #getting the first song form search result
+    #to get all song results use findAll
+    song = soup.find('a', {'class': 'musictitle'})
+    if song.has_attr('href'):
+        #getting link of song
+        song_link=song['href']
+    #converting above to download link 
+    down_link=song_link[:-5]+'_download'+song_link[-5:]
+    #moving to download page
+    parse_download(down_link)
 
-song_result = soup.findAll('div', {'class': 'tenbh'}) #find all songs 
-song = soup.find('a', {'class': 'musictitle'})#find all music 
+def parse_download(down_link):
+    #opening download page
+    download_page=urlopen(down_link)
+    soup = BeautifulSoup(download_page,'html.parser')
+    download_links=()
+    #select links that contains download keyword, here we get 3 links thats why storing them in list
+    download_links=soup.select("a[href*=downloads]")   
+    download(download_links)
 
-
-if song.has_attr('href'):
-    song_link=song['href'] #getting links of songs
-
-down_link=song_link[:-5]+'_download'+song_link[-5:] #converting to download link
-
- 
-d_page=urlopen(down_link)
-soup2 = BeautifulSoup(d_page,'html.parser')
-
-download_links=()
-download_links=soup2.select("a[href*=downloads]")#select links that contains download keyword
-
-
-name = download_links[0]['href'].replace("%20"," ").split('/')[-1]#getting name for downloaded song
-print(name)
-#download file form second link
-link=download_links[0]['href']
-location='songs\\'+name #will download the song in songs folder
-urllib.request.urlretrieve(link,location)
-
+def download(download_links):
+    #download form 1st link
+    link=download_links[0]['href']
+    #name to save
+    name = download_links[0]['href'].replace("%20"," ").split('/')[-1]
+    location='songs\\'+name
+    print("downloading.....\n")
+    #retrieving and saving
+    urllib.request.urlretrieve(link,location)
+    print("Download finished\n")
+    
+   
+if __name__=='__main__':
+    song_name()

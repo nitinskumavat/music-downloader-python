dd9936364ccbde7afebf07ec7f096f6549ecee5e
nitinskumavat <nitin.s.kumavat@gmail.com>
2019-01-29 12:49:35 +0530

    Download form first 5 search result and check if error in fectching site

--- mp3.py
+++ mp3.py
@@ -4,9 +4,17 @@ import urllib.request
 import os
 import re
 
+
 #creating director to add downloaded song
 if not os.path.exists('songs'):
     os.makedirs('songs')
+
+def check_status(link):
+    try:
+        html=urlopen(link)
+        return 1
+    except:
+        return 0
 
 def song_name():
     #input string and convert multiple spaces to single space
@@ -22,27 +30,49 @@ def song_name():
         parse_site(site)
     
 def parse_site(site):
-    page = urlopen(site)
-    soup = BeautifulSoup(page,'html.parser')
-    #getting the first song form search result
-    #to get all song results use findAll
-    song = soup.find('a', {'class': 'musictitle'})
-    if song.has_attr('href'):
-        #getting link of song
-        song_link=song['href']
-    #converting above to download link 
-    down_link=song_link[:-5]+'_download'+song_link[-5:]
-    #moving to download page
-    parse_download(down_link)
+    if check_status(site)==1:
+        page = urlopen(site)
+        soup = BeautifulSoup(page,'html.parser')
+        #getting the first song form search result
+        #to get all song results use findAll
+        song = soup.findAll('a', {'class': 'musictitle'})
+        print("Enter the song number\n")
+        for i in range(5):
+            s = song[i]['href']  
+            lang=s.split("/")[4]#getting language from link
+            song_name=s.split("/")[6].split("~")[0]
+            singer_name=s.split("/")[6].split("~")[1]
+            print("%d) %s, %s, %s" %(i+1,lang,song_name,singer_name))
+        n=input("\nWaiting for input: ")
+        n=n.strip()
+        if n is 'r':
+            song_name()
+        elif n is '1' or n is '2' or n is '3' or n is '4' or n is '5':
+            if song[int(n)-1].has_attr('href'):
+                #getting link of song
+                song_link=song[int(n)-1]['href']
+            down_link=song_link[:-5]+'_download'+song_link[-5:]
+            #moving to download page
+            parse_download(down_link)
+            
+        else:
+            print("Invlaid input\n ")
+    else:
+        print("Http error or Srever not found\n")
+
 
 def parse_download(down_link):
-    #opening download page
-    download_page=urlopen(down_link)
-    soup = BeautifulSoup(download_page,'html.parser')
-    download_links=()
-    #select links that contains download keyword, here we get 3 links thats why storing them in list
-    download_links=soup.select("a[href*=downloads]")   
-    download(download_links)
+     #opening download page
+    if check_status(down_link)==1:       
+        download_page=urlopen(down_link)
+        soup = BeautifulSoup(download_page,'html.parser')
+        download_links=()
+        #select links that contains download keyword, here we get 3 links thats why storing them in list
+        download_links=soup.select("a[href*=downloads]")   
+        download(download_links)
+    else:
+        print("Http error or Srever not found\n")
+        
 
 def download(download_links):
     #download form 1st link
@@ -53,7 +83,7 @@ def download(download_links):
     print("downloading.....\n")
     #retrieving and saving
     urllib.request.urlretrieve(link,location)
-    print("Download finished\n")
+    print("Download complete!\n")
     
    
 if __name__=='__main__':
